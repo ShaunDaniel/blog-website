1e74e41c030e1096fae80785251eff99219553af
s h a u n <73394707+ShaunDaniel@users.noreply.github.com>
2023-07-31 01:10:48 +0530

    updated contact & about constants

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.set('view engine','ejs')
 app.use(express.static("public"))
 
-const aboutContent = "Integer ac nisl condimentum, mollis arcu quis, condimentum dui. In hac habitasse platea dictumst. Donec fringilla, ipsum cursus fringilla sodales, elit elit malesuada nunc, ac malesuada metus dui suscipit sem. Mauris mauris ipsum, tristique at viverra ut, pharetra vitae leo. Suspendisse facilisis sodales dui et dapibus. Sed massa nisl, pharetra non convallis vitae, finibus vitae justo. Cras in lorem et turpis fermentum porttitor eget ut erat."
-const contactContent = "Duis sed auctor velit, in hendrerit eros. Integer egestas tellus a ligula suscipit, quis mattis lorem varius. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dictum mi ac porta ultricies. Praesent vitae efficitur est, et euismod turpis. Proin sollicitudin est et convallis malesuada. Praesent hendrerit turpis orci, sit amet accumsan tellus lobortis eu. Vivamus congue ligula mauris."
+const aboutContent = "A project made as a part of Web Development Bootcamp by Angela Yu"
+const contactContent = "Contact me on - shaun.daniel@science.christuniversity.in."
 
 
 
